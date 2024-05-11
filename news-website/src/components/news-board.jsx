0ad4f2eb949f56ad6c9a7c88a0a6ab181e46cddc
Kamran Zafar <kamranzafar4343@gmail.com>
2024-05-11 17:22:29 +0500

    feat - added navbar from bootstrap

---
+++ news-website/src/components/news-board.jsx
@@ -0,0 +1,9 @@
+
+
+const newsBoard = () => {
+  return (
+    <div>news-board</div>
+  )
+}
+
+export default newsBoard
---
+++ news-website/src/components/news-item.jsx
@@ -0,0 +1,8 @@
+
+const newsItem = () => {
+  return (
+    <div>news-item</div>
+  )
+}
+
+export default newsItem
