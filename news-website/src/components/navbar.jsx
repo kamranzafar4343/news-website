aae86460263a964a64d2b3f72bfde30e18e5ebd4
Kamran Zafar <kamranzafar4343@gmail.com>
2024-05-12 21:55:44 +0500

    updated navbar

--- news-website/src/components/navbar.jsx
+++ news-website/src/components/navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand badge bg-light text-dark fs-4" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><span className=" badge bg-light text-dark fs-4">NewsMagzine</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
