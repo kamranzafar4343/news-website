f94c3da43aab56f06dcd112360e949872fbfca09
Kamran Zafar <kamranzafar4343@gmail.com>
2024-05-16 01:08:54 +0500

    fix - padding in navbar

--- news-website/src/components/navbar.jsx
+++ news-website/src/components/navbar.jsx
@@ -2,31 +2,23 @@ const Navbar = () => {
   return (
 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><span className=" badge bg-light text-dark fs-4">NewsMagzine</span></a>
+    <a className="navbar-brand ps-4" href="#"><span className=" badge bg-light text-dark fs-4">NewsMag</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <li className="nav-item pe-3 ps-5">
+          <a className="nav-link active" aria-current="page" href="#">Science and technology</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+        <li className="nav-item pe-3">
+          <a className="nav-link active " aria-current="page" href="#">Health and Sports</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li className="nav-item pe-3">
+          <a className="nav-link active" aria-current="page" href="#">Research Papers</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        <li className="nav-item pe-3">
+          <a className="nav-link active" aria-current="page" href="#">Politics</a>
         </li>
       </ul>
       
